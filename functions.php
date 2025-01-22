2ac2fbe499212305188d80cd06d95f6b27607c94
KhalimaD <khalima.daudova@eleve.cndbs.be>
2025-01-22 09:49:53 +0100

    fin cours 22.01

--- functions.php
+++ functions.php
@@ -1,5 +1,20 @@
 <?php
 function afficherCartes($listeCartes)
-{ foreach($listeCartes as $carte) 
-          { echo '<img class="carte" src="cartes/'.$carte['image'].'" alt="'.$carte['nom'].'">';}
+{
+    foreach ($listeCartes as $carte) {
+        echo '<img class="carte" src="cartes/' . $carte['image'] . '" alt="' . $carte['nom'] . '">';
+    }
 }
+
+function distribuerCartes()
+{
+    array_merge($cartes) . array_splice($listeCartes, 0, 7);
+}
+
+
+exemple du site: 
+
+$array1 = array("color" => "red", 2, 4);
+$array2 = array("a", "b", "color" => "green", "shape" => "trapezoid", 4);
+$result = array_merge($array1, $array2);
+print_r($result);...
--- header.php
+++ header.php
@@ -8,7 +8,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Jeu UNO - Etape 1</title>
     <link rel="stylesheet" href="style.css">
-    <link rel="shortcut icon" type="image/jpg" href="cartes/uno.png" />
+    <link rel="shortcut icon" type="image/jpg" href="cartes/uno.png" /> <!-- icone en haut page, obli extension png-->
 </head>
 
 <body>
