c955dc0939ed6186c31f99c88732a3933073e794
KhalimaD <khalima.daudova@eleve.cndbs.be>
2025-01-29 09:49:20 +0100

    compteur pr cartes dans doc functions.php le mercredi 29 janv

--- functions.php
+++ functions.php
@@ -1,32 +1,36 @@
 <?php
+
 function afficherCartes($listeCartes)
 {
+    $count = 0;
     foreach ($listeCartes as $carte) {
-        echo '<img class="carte" src="cartes/' . $carte['image'] . '" alt="' . $carte['nom'] . '">';
+        echo '<a href="index.php? ' . $count . '"><img class="carte" src="cartes/' . $carte['image'] . '" alt="' . $carte['nom'] . '"></a>';
+        $count += 1;
     }
 }
 
+
 function afficherCarteSup($defausse)
-    {
-        echo '<img class="carte" src="cartes/' . $defausse[0]['image'] . '" alt="' . $defausse[0]['nom'] . '">';
-    }
+{
+    echo '<img class="carte" src="cartes/' . $defausse[0]['image'] . '" alt="' . $defausse[0]['nom'] . '">';
+}
 
 
 function afficherPioche()
-    {
-        echo '<img class="carte" src="cartes/pioche.jpg" alt="Piocher">';
-    }
+{
+    echo '<img class="carte" src="cartes/pioche.jpg" alt="Piocher">';
+}
 
 
 function distribuerCartes($cartes)
-{    
+{
     //dans cette fonction, j'utilise des variables globales et non locales. 
     for ($i = 0; $i < 7; $i++) {
         $_SESSION['mainJoueur1'] = array_merge($_SESSION['mainJoueur1'], array_splice($cartes, 0, 1));
         $_SESSION['mainJoueur2'] = array_merge($_SESSION['mainJoueur2'], array_splice($cartes, 0, 1));
     }
-    $_SESSION['defausse']= array_splice($cartes, 0, 1);  //on retourne la 1er carte de la pioche
-    $_SESSION['pioche']=$cartes; //les cartes qui restent
+    $_SESSION['defausse'] = array_splice($cartes, 0, 1);  //on retourne la 1er carte de la pioche
+    $_SESSION['pioche'] = $cartes; //les cartes qui restent
 }
 
 
