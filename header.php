180a08344aaab57c9e164cda41b343eb60adc670
Murielle PIRON <murielle.piron@cndbs-binche.be>
2025-03-18 15:21:50 +0100

    intégration du CSS

--- header.php
+++ header.php
@@ -6,14 +6,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Jeu UNO - Etape 1</title>
+    <title>Jeu UNO 2025</title>
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" type="image/jpg" href="cartes/uno.png" /> <!-- icone en haut page, obli extension png-->
 </head>
-<br>
+
 
 <body>
     <nav>
-        Ceci sera un jour le menu du site..... peu de chances.
         <a href="index.php?reset=oui"> Réinitialiser le jeu</a>
     </nav>
