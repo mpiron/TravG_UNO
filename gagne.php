4152f0489b5a0dd4e04fbd1da030ee199a01758c
KhalimaD <khalima.daudova@eleve.cndbs.be>
2025-04-23 09:51:29 +0200

    code fonctionne, travaillé sur formulaire des gagnant

--- gagne.php
+++ gagne.php
@@ -2,20 +2,22 @@
 global $identiteJoueur;
 ?>
 <!DOCTYPE html>
-<html lang="fr">
+
 <head>
     <meta charset="UTF-8">
-    <title>🎉 Félicitations, Vous avez gagné ! 🎉</title>
+    <title>Page du gagnant</title>
     <style>
         /* Style général */
         body {
             font-family: 'Arial', sans-serif;
             /* background: linear-gradient(135deg, #ff6f61, #ffcc00); */
-            background: linear-gradient(to right, rgb(60, 61, 119),rgb(120, 255, 127));
+            background: linear-gradient(to right, rgb(60, 61, 119), rgb(120, 255, 127));
             text-align: center;
             color: white;
-            height: 100vh;  /*vertical height */
-            display: flex;    /* pr créer mise en page s'ajustant autom en fonct taille écran ou contenu */
+            height: 100vh;
+            /*vertical height */
+            display: flex;
+            /* pr créer mise en page s'ajustant autom en fonct taille écran ou contenu */
             justify-content: center;
             align-items: center;
             flex-direction: column;
@@ -26,12 +28,12 @@ global $identiteJoueur;
             margin: 0;
         }
 
-        .btn-rejouer {
+        /* .btn-rejouer {
             margin-top: 30px;
             padding: 15px 30px;
             font-size: 30px;
             background-color: #ffffff;
-            color:rgb(60, 61, 119);
+            color: rgb(60, 61, 119);
             border: none;
             border-radius: 12px;
             cursor: pointer;
@@ -46,14 +48,52 @@ global $identiteJoueur;
 
         .btn-rejouer:active {
             transform: scale(0.9);
-        }
-
+        } */
     </style>
 </head>
+
 <body>
 
     <h1>🎉 Félicitations, le joueur <?php echo $identiteJoueur; ?> a gagné ! 🎉</h1>
-    <button class="btn-rejouer" onclick="window.location.href='index.php'">Rejouer</button>
+
+    <p>Le gagnant veut-il laisser son nom dans le tableau des victorieux?</p>
+
+    <?php
+    // vérifie si form pour réponse oui/non est envoyé
+    if (!empty($_POST['reponse'])) {    //si pas vide
+        if ($_POST['reponse'] === 'oui') {
+
+            // affiche form pour saisir infos(nom)
+    ?>
+            <form method="post" action="gagne.php">
+                <label for="nom">Nom :</label>
+                <input type="text" id="nom" name="nom" required><br><br>
+
+                <input type="submit" value="Envoyer">
+            </form>
+        <?php
+        }
+        if ($_POST['reponse'] === 'non') {
+        ?>
+    <?php
+        }
+    }
+    // vérifier si form complété
+    if (isset($_POST["nom"])) {
+        $nom = $_POST["nom"];
+
+        echo "Nom : " . $nom . "<br>";
+    }
+    ?>
+
+    <!-- form pour appuyer oui/non -->
+    <form method="post">
+        <button type="submit" name="reponse" value="oui">Oui</button>
+    </form>
+    <button onclick="window.location.href='index.php?reset=oui'">non</button>
+
+    <!-- faut récuoérer nom date et tour dans bdd -->
 
 </body>
+
 </html>
